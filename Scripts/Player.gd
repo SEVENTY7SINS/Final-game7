84e6015454af16f3848bb871cde857f5779147cf
SEVENTY7SINS <colenso.king@student.otahuhucollege.school.nz>
2026-08-12 13:54:31 +1200

    Change 1
    I have added more details to the map, I ended this session with trying to limit the player's camera so that it doesn't go off the screen, but its not complete. I have not changed the UI.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var limit = 1
+@onready var number_value = 1
+const zomo = 1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +13,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Up"):
-		position.y -=100
+		position.y -= 10 * limit
 	if Input.is_action_pressed("Down"):
-		position.y +=100
+		position.y += 10  * limit
 	if Input.is_action_pressed("Right"):
-		position.x +=100
+		position.x += 10 * limit
 	if Input.is_action_pressed("Left"):
-		position.x -=100
+		position.x -= 10  * limit
+	
+	position.x = clamp(position.x, 600, 5160)
+	position.y = clamp(position.y, 300, 3150)
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			number_value = min(number_value + .1, 100)
+
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			number_value = max(number_value - .1, 0)

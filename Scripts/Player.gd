08843005f3255896d1264e52cfc772ae84a487b5
SEVENTY7SINS <colenso.king@student.otahuhucollege.school.nz>
2026-08-12 12:50:04 +1200

    I change the speed

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,10 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Up"):
-		position.y -=10
+		position.y -=100
 	if Input.is_action_pressed("Down"):
-		position.y +=10
+		position.y +=100
 	if Input.is_action_pressed("Right"):
-		position.x +=10
+		position.x +=100
 	if Input.is_action_pressed("Left"):
-		position.x -=10
+		position.x -=100
